be11ec5397275eab4ca17b649efa361b4e22ea35
Robby McMurry <rrmcmurry@gmail.com>
2025-09-23 14:45:37 -0400

    Added PhotonVision

---
+++ WorkBench/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+import org.photonvision.PhotonCamera;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSubsystem extends SubsystemBase{
+  // PhotonVision 
+  PhotonCamera camera;
+  public boolean targetVisible;
+  public double targetYaw;  
+  
+
+  public VisionSubsystem() {
+    camera = new PhotonCamera("FrontLeftCamera"); 
+    camera.getLatestResult(); // warm-up  
+  }
+
+  
+  public void teleopPeriodic(){
+    targetVisible = false;
+    targetYaw = 0.0;
+    var results = camera.getAllUnreadResults();
+    if (!results.isEmpty()) {
+      var result = results.get(results.size() - 1);
+      if (result.hasTargets()){
+        for (var target : result.getTargets()) {
+          if (target.getFiducialId() == 1) {
+            targetVisible = true;
+            targetYaw = target.getYaw();
+          }
+        }
+      }
+    }    
+  }
+}
